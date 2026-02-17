c112bfa05befa85e8f09a8f79abf342e0130a24b
lujoel87 <lujoel87@gmail.com>
2026-02-17 02:49:30 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Le quitamos el .tsx al final para que sea más compatible
+import App from './App' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
